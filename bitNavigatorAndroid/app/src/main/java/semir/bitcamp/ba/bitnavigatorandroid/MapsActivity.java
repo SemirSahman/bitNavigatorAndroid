e1a4a76ffab67c92c535f65bdd16e1b9cfdd8726
Hajrudin Sehic <hajrudin.sehic@BitCamps-Mac-mini-9.local>
2015-10-22 16:20:06 +0200

    aa

--- bitNavigatorAndroid/app/src/main/java/semir/bitcamp/ba/bitnavigatorandroid/MapsActivity.java
+++ bitNavigatorAndroid/app/src/main/java/semir/bitcamp/ba/bitnavigatorandroid/MapsActivity.java
@@ -25,7 +25,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
+        
         TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
 
         loginScreen.setOnClickListener(new View.OnClickListener() {
